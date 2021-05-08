feb79b2076e49e2b2e46213e3880e9adca3eb25c
PSOrigins <serapinaspijus@gmail.com>
2021-05-08 18:47:06 +0300

    Added chunking, react lazy and suspense to project pages

--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.styles.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.styles.jsx
@@ -5,4 +5,8 @@ export const SignInAndSignUpContainer = styled.div`
   display: flex;
   justify-content: space-between;
   margin: 30px auto;
+  @media screen and (min-width: 800px) {
+    display: gird;
+    grid-template-columns: 1fr;
+  }
 `;
